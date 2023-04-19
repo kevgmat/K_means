04346e465306c8f2d862679fcaea0e30ca24bb23
kevin george mathew <kevgmat@gmail.com>
2023-04-19 18:35:42 +0530

    2 dimension test

--- K_means.py
+++ K_means.py
@@ -25,6 +25,80 @@ class KMean():
         self.clusters = [[] for i in range(self.K)]
         self.centroids = []
 
+    def predict(self, input_data):
+        self.input_data = input_data
+        self.samples, self.dimensions = input_data.shape
+
+        random_indexes = np.random.choice(self.samples, self.K, replace = False)
+        self.centroids = [self.input_data[index] for index in random_indexes]
+
+        for i in range(self.max_iterations):
+            self.clusters = self.create_clusters(self.centroids)
+
+            if self.steps:
+                self.plot()
+
+            old_centroids = self.centroids
+            self.centroids = self.get_centroids(self.clusters)
+
+            if self.is_converged(old_centroids, self.centroids):
+                break
+            if self.steps:
+                self.plot()
+
+        return self.get_cluster_labels(self.clusters)
+
+
+
+    def get_cluster_labels(self, clusters):
+        labels = np.empty(self.n_samples)
+
+        for cluster_index, cluster in enumerate(clusters):
+            for sample_index in cluster:
+                labels[sample_index] = cluster_index
+        return labels
+
+    def create_clusters(self, centroids):
+        clusters = [[] for i in range(self.K)]
+        for index, sample in enumerate(self.input_data):
+            centroid_index = self.closest_centroid(sample, centroids)
+            clusters[centroid_index].append(index)
+        return clusters
+
+    def closest_centroid(self, sample, centroids):
+        distances = [distance(sample, point) for point in centroids]
+        closest_index = np.argmin(distances)
+        return closest_index
+
+    def get_centroids(self, clusters):
+        centroids = np.zeros((self.K, self.dimensions))
+        for cluster_index, cluster in enumerate(clusters):
+            cluster_mean = np.mean(self.input_data[cluster], axis = 0)
+            centroids[cluster_index] = cluster_mean
+        return centroids
+
+    def is_converged(self, old_centroids, centroids):
+        distances = [distance(old_centroids[i], centroids[i]) for i in range(self.K)]
+        return sum(distances) == 0
+
+    def plot(self):
+
+        fig, ax = plt.subplots(figsize = (12,8))
+
+        for i, index in enumerate(self.clusters):
+            point = self.input_data[index].T
+            ax.scatter(*point)
+
+        for point in self.centroids:
+            ax.scatter(*point, marker = "x", color = 'black', linewidth = 2)
+        plt.show()
+
+sample_data = np.random.randn(100,2)
+print(sample_data)
+model = KMean()
+model.predict(sample_data)
+
+
 
 
 # import pandas as pd
